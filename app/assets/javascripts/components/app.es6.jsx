349143e0e4088e9538c08e3fba244d281fea9e03
David Jiang <davidjgrey@gmail.com>
2017-03-13 18:12:46 -0700

    geolocation added

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -7,19 +7,45 @@ class App extends React.Component {
 		this.makeMarker = this.makeMarker.bind(this);
 		this.makeInfoWindow = this.makeInfoWindow.bind(this);
 		this.makeInfoWindowText = this.makeInfoWindowText.bind(this);
+		this.initializeMap = this.initializeMap.bind(this);
+		this.handleLocationError = this.handleLocationError.bind(this);
 	}
 	componentDidMount() {
+		this.initializeMap();
+	}
+	initializeMap(mapCenter){
 		//create map
 		this.map = new google.maps.Map(this.refs.map, {
 			//Downtown Seattle
           center: {lat: 47.6063889, lng: -122.3308333},
-          zoom: 14
+          zoom: 12
         });
+		let infoWindow = new google.maps.InfoWindow();
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				let pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+				this.map.setCenter(pos);
+				infoWindow.setPosition(pos);
+				infoWindow.setContent('You are here');
+				infoWindow.open(this.map);
+			}, () => {
+				this.handleLocationError(true, infoWindow, this.map.getCenter());
+			});
+		}
+		else{
+			this.handleLocationError(false, infoWindow, this.map.getCenter());
+		}
 		//create marker and info window
 		this.props.locations.forEach((location) => {
 			let marker = this.makeMarker(location);
-
 		});
+	}
+	handleLocationError (browserHasGeolocation, infoWindow, pos){
+		infoWindow.setPosition(pos);
+		infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Your browser doesn\'t support geolocation.');
 	}
 	makeMarker (location){
 		let marker = new google.maps.Marker({
@@ -56,7 +82,7 @@ class App extends React.Component {
 		const mapStyle = {
 			height: '100%'
 		}
-    return <div ref="map" style={mapStyle}></div>;
+    return <div ref="map" style={mapStyle}>Finding your location...</div>;
   }
 }
 
