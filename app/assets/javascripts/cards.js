45393620ca47ddea55023f3e7c46ab41f80e5949
David Jiang <davidjgrey@gmail.com>
2017-04-02 13:10:24 -0700

    removed cards js

--- app/assets/javascripts/cards.js
+++
@@ -1,8 +0,0 @@
-/* jshint strict: false, asi: true, esversion:6 */
-$(document).ready(function(){
-	$(".card-name").on('ajax:success', function(e){
-		console.log("Clicked!");
-		let id = e.target.attr('data-id');
-		$(`.card-details[data-id=${id}]`).slideToggle();
-	})
-});
